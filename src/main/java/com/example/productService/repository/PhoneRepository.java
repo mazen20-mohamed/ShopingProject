ef6ba4bfc19673d9ccb3511674b94523d44075e7
Mazen Mohamed <eldessoukymazen16@gmail.com>
2024-05-31 16:19:35 +0300

    newww

--- src/main/java/com/example/productService/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productService.repository;
-
-import com.example.productService.model.shop.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone,Long> {
-}
